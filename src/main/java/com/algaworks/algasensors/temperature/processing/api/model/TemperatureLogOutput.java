97f33ca54288fca950ee54a209304f0fc684920d
RE045908 <re045908@qintess.com>
2025-05-04 11:52:42 -0300

    created TemperatureLogOutput.java

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/model/TemperatureLogOutput.java
@@ -0,0 +1,22 @@
+package com.algaworks.algasensors.temperature.processing.api.model;
+
+import io.hypersistence.tsid.TSID;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class TemperatureLogOutput {
+
+    private UUID id;
+
+    private TSID sensorID;
+
+    private OffsetDateTime registeredAt;
+
+    private Double valeu;
+
+}
